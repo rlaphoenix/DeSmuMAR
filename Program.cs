2a20058845a29b6eae8e8616a243980fbdfcb3d4
PRAGMA <pragma.exe@gmail.com>
2019-05-21 20:25:45 +0100

    v1.2

--- Program.cs
+++ Program.cs
@@ -21,6 +21,12 @@ namespace DeSmuME_Resizer {
 
 		[DllImport("user32.dll", SetLastError = true)]
 		static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int Width, int Height, bool Repaint);
+		[DllImport("user32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, int uFlags);
+		private const int HWND_TOPMOST = -1;
+		private const int SWP_NOMOVE = 0x0002;
+		private const int SWP_NOSIZE = 0x0001;
 		#endregion
 		#region Constants
 		const string SETTINGS_FILE = "DeSmuME Resizer.ini";
@@ -41,7 +47,7 @@ namespace DeSmuME_Resizer {
 			#region Run DeSmuME.exe
 			Process p = new Process() {
 				StartInfo = new ProcessStartInfo() {
-					FileName = "DeSmuME.exe"
+					FileName = "C:/Program Files/DeSmuME/DeSmuME.exe"
 				}
 			};
 			p.Start();
@@ -52,17 +58,32 @@ namespace DeSmuME_Resizer {
 				IntPtr handle = p.MainWindowHandle;
 				Console.WriteLine("Got Window Handle");
 				RECT Rect = new RECT();
-				while (!GetWindowRect(handle = p.MainWindowHandle, ref Rect)) {
+				while (!GetWindowRect(handle, ref Rect)) {
 					Thread.Sleep(10);
 					Console.WriteLine("Failed to get Handle's Bounds, retrying...");
 				}
 				Console.WriteLine("Got Window Bounds");
-				while (!MoveWindow(handle = p.MainWindowHandle, Rect.left, Rect.top, Width + 16, Height + 59, true)) {
+				while (!MoveWindow(handle, Rect.left, Rect.top, Width + 16, Height + 59, true)) {
 					Console.WriteLine("Failed to resize Handle for some reason, retrying...");
 				}
 				Console.WriteLine("Resized Window");
-				if (Rect.right - Rect.left == Width + 16 && Rect.bottom - Rect.top == Height + 59) {
+				while (!GetWindowRect(handle, ref Rect)) {
+					Thread.Sleep(10);
+					Console.WriteLine("Failed to update Handle's Bounds, retrying...");
+				}
+				Console.WriteLine("Updated Window Bounds");
+				int ResultingHeight = Rect.bottom - Rect.top;
+				int ResultingWidth = Rect.right - Rect.left;
+				if (ResultingWidth == Width + 16 && ResultingHeight == Height + 59) {
 					Console.WriteLine("Resize verified to be correct, nice!");
+					break;
+				}
+				if(ResultingHeight == 1019 && ResultingWidth == 2576) {
+					Console.ForegroundColor = ConsoleColor.DarkRed;
+					Console.BackgroundColor = ConsoleColor.White;
+					Console.WriteLine("DeSmuME has View -> \"Maintain Aspect Ratio\" enabled, this needs to be disabled.");
+					SetWindowPos(Process.GetCurrentProcess().MainWindowHandle, new IntPtr(HWND_TOPMOST), 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+					Console.ReadLine();
 					break;
 				}
 				Console.WriteLine("Something isn't quite right, everything went succesful except for the verification of the resulting window bounds, retrying in 2 seconds...");
