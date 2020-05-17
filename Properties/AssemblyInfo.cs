ba40e1abdfb633869744a448c79a87e0c6288d90
PRAGMA <17136956+imPRAGMA@users.noreply.github.com>
2020-05-17 23:11:21 +0100

    v1.5.2 - Update DEV release CI links and exectuable path

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DeSmuMAR")]
 [assembly: AssemblyDescription("An automated aspect ratio based resizing method for DeSmuME for playing in specific Aspect Ratio's.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PHOENiX@GIT")]
 [assembly: AssemblyProduct("DeSmuMAR")]
-[assembly: AssemblyCopyright("Copyright © PRAGMA 2019")]
+[assembly: AssemblyCopyright("Copyright © PHOENiX 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.2.0")]
